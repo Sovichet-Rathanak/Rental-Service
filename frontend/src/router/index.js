5c1cf4fcf5c43c8b08e94a154f8132b62e966675
yungNita <yung.nita1306@gmail.com>
2025-05-13 19:39:17 +0700

    update route & Wishlist not finish yet

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -12,6 +12,7 @@ import InvoicePage from '@/views/InvoicePage.vue'
 import RentingList from '@/views/RentingList.vue'
 import PaymentPage from '@/views/PaymentRelated/PaymentPage.vue'
 import ManagePayment from '@/views/PaymentRelated/ManagePayment.vue'
+import WishlistPage from '@/views/WishlistPage.vue'
 
 const routes = [
   {
@@ -68,6 +69,11 @@ const routes = [
     path: '/personalInfo',
     name: 'personalInfo',
     component: PersonalInfoPage,
+  },
+  {
+    path: '/wishtlist',
+    name: 'WishList',
+    component: WishlistPage,
   },
 ]
 
